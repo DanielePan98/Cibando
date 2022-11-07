ba89fe31a838484e82dca0af9cdb7bb4fd2caa54
DanielePan98 <danielepan98@gmail.com>
2022-11-07 17:25:14 +0100

    Primo commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
+import { faHouzz } from '@fortawesome/free-brands-svg-icons';
+import { faRegistered } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cibando';
+  iconaHome = faHouzz;
+  iconaScheda = faNewspaper;//ricette
+  iconaMail = faMailBulk;
+  iconaRegistrato = faRegistered;
+  images = [
+    {
+      id: 1,
+      label: "Spaghetti al sugo"
+    },
+    // {
+    //   id: 2,
+    //   label: "tagliata di manzo"
+    // },
+    // {
+    //   id: 3,
+    //   label: "tiramisu classico"
+    // }
+  ];
+
+  percorso = '../assets/images/carousel-';
+
+  showNavigation() {
+    if (this.images.length > 1) {
+     return true;
+    }
+    return false;
+  }
+
+}
